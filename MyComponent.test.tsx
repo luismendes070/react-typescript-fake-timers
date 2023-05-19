3864d9e97e29cdfcae5efebc3d78928a5cbebba8
luismendes070 <machadoluismendes@gmail.com>
2023-05-19 05:24:16 -0300

    1 file modified

--- MyComponent.test.tsx
+++ MyComponent.test.tsx
@@ -1,3 +1,43 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
+
+describe('MyComponent', () => {
+  let container: HTMLDivElement | null = null;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+    jest.useFakeTimers(); // Enable fake timers
+  });
+
+  afterEach(() => {
+    if (container) {
+      unmountComponentAtNode(container);
+      container.remove();
+      container = null;
+    }
+    jest.useRealTimers(); // Restore real timers
+  });
+
+  it('renders without crashing', () => {
+    act(() => {
+      render(<MyComponent />, container);
+    });
+    // Your assertions here
+  });
+
+  it('performs a delayed action', () => {
+    act(() => {
+      render(<MyComponent />, container);
+    });
+
+    act(() => {
+      // Simulate a delay of 1000ms
+      jest.advanceTimersByTime(1000);
+    });
+
+    // Your assertions here
+  });
+});
+
